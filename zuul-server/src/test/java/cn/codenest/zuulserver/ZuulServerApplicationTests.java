bd93cb803e8fb3ae02c2d7fc8a98a550e65ac59b
HymanLiuTS <879651072@qq.com>
2020-12-24 14:00:51 +0800

    提交路由zuul相关代码

---
+++ zuul-server/src/test/java/cn/codenest/zuulserver/ZuulServerApplicationTests.java
@@ -0,0 +1,11 @@
+package cn.codenest.zuulserver;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZuulServerApplicationTests {
+
+    void contextLoads() {
+    }
+
+}
