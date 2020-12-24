5889a309f7fdbbf9b42565a0922dcfd84e1b0698
HymanLiuTS <879651072@qq.com>
2020-12-24 19:03:33 +0800

    提交路由zuul相关代码

--- zuul-server/src/main/java/cn/codenest/zuulserver/ZuulServerApplication.java
+++ zuul-server/src/main/java/cn/codenest/zuulserver/ZuulServerApplication.java
@@ -1,5 +1,6 @@
 package cn.codenest.zuulserver;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
@@ -7,6 +8,9 @@ import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 @EnableZuulProxy
 @SpringBootApplication
 public class ZuulServerApplication {
+
+    @Value("${license.autocontrol}")
+    private String autoLicense;
 
     public static void main(String[] args) {
         SpringApplication.run(ZuulServerApplication.class, args);
