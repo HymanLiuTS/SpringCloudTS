27f7e785974e676bb44708d94da7480f76d1ea5a
HymanLiuTS <879651072@qq.com>
2021-01-19 12:24:33 +0800

    增加zipserver的说明

--- zipkin-server/src/main/java/cn/codenest/zipkinserver/ZipkinServerApplication.java
+++ zipkin-server/src/main/java/cn/codenest/zipkinserver/ZipkinServerApplication.java
@@ -3,8 +3,9 @@ package cn.codenest.zipkinserver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
-@EnableZipkinStreamServer
+//todo 官网已经不再推荐自己编译ZIPSERVER，相反，建议直接下载jar
+// 若非要自己搭建，可以参考https://blog.csdn.net/setlilei/article/details/102961605
+//@EnableZipkinServer
 @SpringBootApplication
 public class ZipkinServerApplication {
 
